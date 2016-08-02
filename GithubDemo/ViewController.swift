c124e1dc3a2ec8c1660fc6297c99f8ab4e006004
bkkoo <bon@duotrac.com>
2016-08-02 16:09:08 +0530

    A new project is created for Github Demo source code demo app.

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Hi inside of viewWillAppear method of GithubDemo project.")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
